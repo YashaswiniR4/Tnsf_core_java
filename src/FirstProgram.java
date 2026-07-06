37aa24f5b34c400285a9516edb74429fae684c58
Yashu <yashaswinir483@gmail.com>
2026-07-06 15:08:27 +0530

    First Java Program

---
+++ src/FirstProgram.java
@@ -0,0 +1,14 @@
+
+public class FirstProgram {
+
+//	public FirstProgram() {
+//		// TODO Auto-generated constructor stub
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! ");
+
+	}
+
+}
